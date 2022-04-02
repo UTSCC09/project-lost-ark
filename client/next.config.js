31a35313c458f282ad7217355a82f08e6b6995df
Gary Xie <garyxie13@live.com>
2022-04-02 13:52:03 -0400

    Debugging commit

--- client/next.config.js
+++ client/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     return [
       { 
         source: "/api/:path*",
-        destination: `${process.env.BACKEND_HOST}/:path*`
+        destination: `https://${process.env.BACKEND_HOST}/:path*`
       }
     ]
   }
